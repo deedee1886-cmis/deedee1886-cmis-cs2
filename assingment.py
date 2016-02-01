2ccab566c93036793265439346b28428d5496c18
cmis <cmis@cmis-hs07.(none)>
2016-02-01 13:35:52 +0700

    Added more new info on assingment.py

--- assingment.py
+++ assingment.py
@@ -1,8 +1,19 @@
 MyName = "DeeDee"
 MyAge = 14.0
-MyHeight = 1.7018
-LengthOf1SideOfSquare = 
-LengthOfTriangle = 
-HeightOfTriangle
+MyHeightInMeters = 1.7018
+LengthOf1SideOfSquareInMeters = 8
+LengthOfTriangleInMeters = 6
+HeightOfTriangleInMeters = 5
 MyAgeInMonths = MyAge*12
+YearsLeftOnEarth = 65 
+MyHeightInFeet = 5.5
+AverageHeightInMeters = 1.52
+TheDifferenceBetweenMyHeightInMetersVSAverageHeightInMeters = MyHeightInMeters-AverageHeightInMeters
+AreaOfSquareInMeters = 64
+VolumeOfCube = AreaOfSquareInMeters*32
+OneNinthOfTheAreaOfTriangleInMeters = 1.6
+print "My name is" + str(MyName) + "I am" + str(MyAge) + "I am" str(MyHeightInMeters) + "Tall" + "I have" str(YearsLeftOnEarth) + "years left on earth" + "I am" str(TheDifferenceBetweenMyHeightInMetersVSAverageHeightInMeters = MyHeightInMeters-AverageHeightInMeters) "Meters taller than the average 14 year old asian male"
+
+
+
 
