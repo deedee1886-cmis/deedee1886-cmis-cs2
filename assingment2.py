98744ce759c890486a98854fd516998a3a6fb60c
cmis <cmis@cmis-hs07.(none)>
2016-02-17 13:34:38 +0700

    created simpleprogram.py

--- assingment2.py
+++ assingment2.py
@@ -19,8 +19,17 @@ def hours_from_seconds(a,b, c):
 	return a/b/c
 hours_from_seconds(86400,60,60)
 
-def circle_area(a, b):
-	return 
+Areaofcircle=(5)
+def volume_of_sphere(r):
+	pi = (math.pi)
+	Vol1 = (((4/3.0)*pi)*a**3)
+	Vol2 = (((4/3.0)*pi)*b**3)
+	return Vol1 + Vol2/2
+
+averagevolume(5,10)
+def areaoftriangel(a,b,c):
+	p = (a+b+c)/2
+	return math.sqrt(p(p−a)(p−b)(p−c))
 
 	
 
---
+++ simpleprogram.py
@@ -0,0 +1,12 @@
+def info(a, b):
+	a = raw_input("type your height here")
+	return a
+def height_in_inches():
+	info(b)
+	height_in_inches = a * 0.39370
+
+def your_height_vs_average_height():
+	info(c)
+	return your height_vs_average_height = height_in_inches - 69
+
+print "you are" + b + "inches tall" + c + "this is your height compared to the average height (69 inches)" 
