820d27b472d8323a527f5ac9f076d95d14dae37d
cmis <cmis@cmis-hs07.(none)>
2016-02-25 13:35:48 +0700

    smallest change lmao

--- simpleprogram.py
+++ simpleprogram.py
@@ -10,7 +10,7 @@ def your_height_vs_average_height():
 	your_height_vs_average_height = float(height_in_inches()) - 69
 	return your_height_vs_average_height
 
-print "you are " + str(height_in_inches()) + " inches tall " + str(your_height_vs_average_height()) + " is your height compared to the average height (69 inches)" 
+print "you are " + str(height_in_inches()) + " inches tall, " + str(your_height_vs_average_height()) + " is your height compared to the average height (69 inches)" 
 #if the number returned is positive, that means you are that much taller than the average height, if the number returned is negative, then that means you are that much shorter than average height
 
 
