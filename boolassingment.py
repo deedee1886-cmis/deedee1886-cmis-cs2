a8a9155d1d1056fb3d8b073789a1b351e227069a
cmis <cmis@cmis-hs07.(none)>
2016-03-07 10:56:56 +0700

    started new project

---
+++ boolassingment.py
@@ -0,0 +1,11 @@
+
+#Stranded on an island is a program that will ask the user to make many choices, their choices will determine their survival on this uninhibited island.
+
+a = raw_input("type your name here ")
+print "Helcome to, Stranded on an Island"
+print "Hello " + str(a) + ", your ship have crashed and you have been stranded on island."
+print "Here you will have many choices to make, remember your survival depends on those choices."
+print "You see that both your knife and canteen have washed up onto shore with you, but you can only take one, which one do you pick?"
+raw_input("type your choice here ")
+knife = True
+canteen = False
