fe94c0b1879ba338e3de9665bfd54ae152635156
cmis <cmis@cmis-hs17.(none)>
2016-03-17 08:40:39 +0700

    almost done

--- guessgame.py
+++ guessgame.py
@@ -2,29 +2,29 @@ V1 = raw_input("type minimal number ")
 V2 = raw_input("type maxium number ")
 
 import random
-def target(V1,V2): 
+def Target(V1,V2): 
 	Target = (random.randint(int(V1),int(V2))) 
 	return Target
 
-print "I'm thinking of a number from " + V1 + " to " + V2
+print "I'm thinking of a number from " + int(V1) + " to " + int(V2)
 
 print "What do you think that number is?"
 
 user_guess = raw_input("type your guess ")
 
-print "The target was " + str(target(V1,V2))
+print "The target was " + str(Target(V1,V2))
 
-print "Your guess was " + user_guess
+print "Your guess was " + str(user_guess)
 
 
-if user_guess > target:
-	result1 = user_guess - target
-	print "That's over by" + result1
+if user_guess > (Target(V1,V2)):
+	result1 = int(user_guess) - (Target(V1,V2))
+	print "That's over by " + str(result1)
 
 else:
-	if user_guess < target:
-		result2 = target - user_guess
-	print "That's under by" + result2
+	 user_guess < (Target(V1,V2))
+	result2 = (Target(V1,V2)) - int(user_guess)
+	print "That's under by " + str(result2)
 
-
-
+if user_guess == Target:
+	print "Great guess, you are correct"
