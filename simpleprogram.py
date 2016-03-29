e82f3de7a4b087eef09fea845e56718a6d67eeec
cmis <cmis@cmis-hs17.(none)>
2016-03-29 13:36:21 +0700

    work in progress

--- simpleprogram.py
+++ simpleprogram.py
@@ -1,5 +1,9 @@
 
-print "Hello " + first_name +' '+ last_name + " nice to meet you, welcome to the height conversion program"
+first_name = raw_input("Type your name here ")
+last_name = raw_input("Type your last name here ")
+
+print "Hello " + first_name +' '+last_name + " nice to meet you, welcome to the height conversion program"
+
 
 def info():
 	a = raw_input("type your height here: ")
@@ -14,8 +18,6 @@ def your_height_vs_average_height():
 	return your_height_vs_average_height
 
 def main():
-	first_name = raw_input("Type your name here ")
-	last_name = raw_input("Type your last name here ")
 	print "you are {} inches tall,{} is your height compared to the average height (69 inches)".format(str(height_in_inches()),str(your_height_vs_average_height())) 
 #if the number returned is positive, that means you are that much taller than the average height, if the number returned is negative, then that means you are that much shorter than average height
 main()
