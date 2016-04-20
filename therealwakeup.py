e73850df6bab9859ba3c06bfa8b7a25e209d1342
cmis <cmis@cmis-hs19.(none)>
2016-04-20 08:37:24 +0700

    still stuck

--- therealwakeup.py
+++ therealwakeup.py
@@ -1,16 +1,22 @@
-
+import numpy as np
 print "This program will ask you for 5 integer or float value. It will calulate the average of all values from 0 inclusive to 10 exclusive. It will print out whether the resulting average is even or odd."
 
-def numbers():
-	first = raw_input("n0: ")
-	second = raw_input("n1: ")
-	third = raw_input("n2: ")
-	fourth = raw_input("n3: ")
-	fith = raw_input("n4: ")
+first = raw_input("n0: ")
+second = raw_input("n1: ")
+third = raw_input("n2: ")
+fourth = raw_input("n3: ")
+fith = raw_input("n4: ")
 
-numbers()
+if first >= 10:
+	print "that number is out of range"
 
-the_average = first + second + third + fourth + fith / 4
+print np.mean(first, second, third, fourth, fith)
+	
+	
+
+
+
+
 
 
 
