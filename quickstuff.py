7de0190f0c3d9a5d7b59e69058d69ad996965e21
cmis <cmis@cmis-hs12.(none)>
2016-05-24 13:35:24 +0700

    new things to learn

---
+++ quickstuff.py
@@ -0,0 +1,31 @@
+#While <boolean expression:
+	#block of code
+
+#while False:
+	#print ":)"
+
+
+def countdown(x = 10):
+    while x >= 0: 
+        print x
+        x -= 1
+countdown()
+
+
+def countup(x = 0):
+    while x >= 10:
+        print x
+        x += 1
+countup()
+
+
+def count(x = 10):
+    if x >= 0:
+        print x
+        x -= 1
+
+    else:
+        x >= 0:
+        print x
+        x += 1
+        
