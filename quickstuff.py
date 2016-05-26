35b2c829995983d92e213be2b7a5f05e6300e9a4
cmis <cmis@cmis-hs13.(none)>
2016-05-26 08:40:24 +0700

    impossiburu

--- quickstuff.py
+++ quickstuff.py
@@ -5,27 +5,48 @@
 	#print ":)"
 
 
-def countdown(x = 10):
-    while x >= 0: 
-        print x
-        x -= 1
-countdown()
+#def countdown(x = 10):
+    #while x >= 0: 
+        #print x
+        #x -= 1
+#countdown()
 
 
-def countup(x = 0):
-    while x >= 10:
-        print x
-        x += 1
-countup()
+#def countup(x = 0):
+    #while x >= 10:
+        #print x
+        #x += 1
+#countup()
 
 
-def count(x = 10):
-    if x >= 0:
-        print x
-        x -= 1
+#def count(x = 10):
+    #if x >= 0:
+        #print x
+       # x -= 1
 
-    else:
-        x >= 0:
-        print x
-        x += 1
+  #  else:
+   #     x >= 0
+     #   print x
+   #     x += 1
+
+
+
+#def counter(x = 1, y = 10):
+	#if x > y:
+#		print x 
+		#x += 1
+	##else:
+		#x < y
+		#print x
+		#x -= 1 
+
+
+def odds(x = 1, y = 10):
+	if x <= y:
+		while x <= y:
+			print x 
+			print x += 1
+
+
+
         
